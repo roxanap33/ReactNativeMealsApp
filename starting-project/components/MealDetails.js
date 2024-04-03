a63df6e43be2e9ef444932cf0c5b9c99d96979c6
roxanap33 <roxana.ciocian@principal33.com>
2024-04-03 11:23:16 +0300

    105: Create a reusable component for meal details

---
+++ starting-project/components/MealDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function MealDetails({ duration, complexity, affordabilty }) {
+  return (
+    <View style={styles.details}>
+      <Text style={styles.detailItem}>{duration}m</Text>
+      <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+      <Text style={styles.detailItem}>{affordabilty.toUpperCase()}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
